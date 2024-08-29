777a0d98449921f53eea3907e0930373ae3f3f0b
Ev357 <evest.dev@gmail.com>
2024-08-30 00:07:18 +0200

    fix: moved keymap to plugin file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -178,17 +178,6 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
-      on_attach = function(bufnr)
-        local gitsigns = require 'gitsigns'
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        map('n', '<leader>gB', gitsigns.toggle_current_line_blame, { desc = '[G]it [B]lame' })
-      end,
     },
   },
 
