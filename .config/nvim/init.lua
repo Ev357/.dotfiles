c5123ce5d2c2ec7a376914bbdd41bcb24432851a
Ev357 <evest.dev@gmail.com>
2024-09-21 17:03:00 +0200

    feat: added some additional file types for prettier

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -679,7 +679,7 @@ require('lazy').setup({
           -- You can use 'stop_after_first' to run the first available formatter from the list
         }
 
-        for _, lang in ipairs { 'javascript', 'typescript', 'vue', 'html' } do
+        for _, lang in ipairs { 'javascript', 'typescript', 'vue', 'html', 'css', 'json' } do
           config[lang] = { 'prettierd', 'prettier', stop_after_first = true }
         end
 
