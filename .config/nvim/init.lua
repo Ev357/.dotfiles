07b1295d17142042afcd6fb1e30eea8c51a7dc51
lucas.gager <lucas.gager@anafra.cz>
2024-09-24 15:58:01 +0200

    refactor: found the real culprit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -105,12 +105,8 @@ vim.keymap.set('v', '<C-j>', ":m '>+1<CR>gv", { desc = 'Move line down' })
 vim.keymap.set('v', '<C-Up>', ":m '<-2<CR>gv", { desc = 'Move line up' })
 vim.keymap.set('v', '<C-Down>', ":m '>+1<CR>gv", { desc = 'Move line down' })
 
-vim.keymap.set('n', '<PageUp>', '')
-vim.keymap.set('n', '<PageDown>', '')
-vim.keymap.set('v', '<PageUp>', '')
-vim.keymap.set('v', '<PageDown>', '')
-vim.keymap.set('i', '<PageUp>', '')
-vim.keymap.set('i', '<PageDown>', '')
+vim.keymap.set('v', '<S-Up>', 'k')
+vim.keymap.set('v', '<S-Down>', 'j')
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
