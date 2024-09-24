b72c48113aaff52ae8ec471b8e7ab5d42c03e076
lucas.gager <lucas.gager@anafra.cz>
2024-09-24 17:04:44 +0200

    refactor: disabled supermaven messages entirely

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -149,7 +149,7 @@ require('lazy').setup({
     'supermaven-inc/supermaven-nvim',
     config = function()
       require('supermaven-nvim').setup {
-        log_level = 'warn',
+        log_level = 'off',
       }
     end,
   },
