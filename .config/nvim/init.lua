1e5f6f0f48e25e5579c2165f5fe810e8cc079662
lucas.gager <lucas.gager@anafra.cz>
2024-12-15 19:35:14 +0100

    fix: renaming

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -531,9 +531,65 @@ require('lazy').setup({
           --  Similar to document symbols, except searches over your entire project.
           map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
+          local lsp_have_feature = {
+            rename = function(client)
+              return client.supports_method 'textDocument/rename'
+            end,
+            inlay_hint = function(client)
+              return client.supports_method 'textDocument/inlayHint'
+            end,
+          }
+
+          local function get_lsp_client_names(have_feature)
+            local client_names = {}
+            local attached_clients = vim.lsp.get_clients { bufnr = 0 }
+            for _, client in ipairs(attached_clients) do
+              if have_feature(client) then
+                table.insert(client_names, client.name)
+              end
+            end
+            return client_names
+          end
+
+          local function lsp_buf_rename(client_name)
+            vim.lsp.buf.rename(nil, { name = client_name })
+          end
+
+          local function lsp_buf_rename_use_one(fallback)
+            local client_names = get_lsp_client_names(lsp_have_feature.rename)
+            if #client_names == 1 then
+              lsp_buf_rename(client_names[1])
+              return
+            end
+            if fallback then
+              fallback()
+            end
+          end
+
+          local function lsp_buf_rename_use_select(fallback)
+            local client_names = get_lsp_client_names(lsp_have_feature.rename)
+            local prompt = 'Select lsp client for rename operation'
+            local function on_choice(client_name)
+              if client_name then
+                lsp_buf_rename(client_name)
+                return
+              end
+              if fallback then
+                fallback()
+              end
+            end
+            vim.ui.select(client_names, { prompt = prompt }, on_choice)
+          end
+
+          local function lsp_buf_rename_use_priority_or_select()
+            lsp_buf_rename_use_one(function()
+              lsp_buf_rename_use_select()
+            end)
+          end
+
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('<leader>rn', lsp_buf_rename_use_priority_or_select, '[R]e[n]ame')
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
