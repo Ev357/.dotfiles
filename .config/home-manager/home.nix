365c2ce27ef5f526204555073a5b91678aea6776
Ev357 <evest.dev@gmail.com>
2025-03-05 18:58:06 +0100

    refactor: moved otd presets to hm

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -27,6 +27,7 @@
   home.file = {
     ".config/electron-flags.conf".source = ./dotfiles/.config/electron-flags.conf;
     ".config/backgrounds/".source = ./dotfiles/.config/backgrounds;
+    ".config/OpenTabletDriver/Presets/".source = ./dotfiles/.config/OpenTabletDriver/Presets;
     ".icons/hypr_Bibata-Modern-Classic/".source = builtins.fetchTarball {
       url = "https://github.com/LOSEARDES77/Bibata-Cursor-hyprcursor/releases/download/1.0/hypr_Bibata-Modern-Classic.tar.gz";
       sha256 = "08q5l2sywc0s70zdn7jvr0rbzz0w8j18wmlkf2x1l64y93lbvzsj";
