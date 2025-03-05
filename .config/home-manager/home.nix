0f7a6d9adfeaf28b019bf33db2cb2adcc56f0b24
Ev357 <evest.dev@gmail.com>
2025-03-05 23:28:13 +0100

    refactor: moved nix to hm

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -89,5 +89,16 @@
       size = 24;
     };
   };
+
+  nix = {
+    package = pkgs.nixVersions.latest;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  nixpkgs = {
+    config.allowUnfree = true;
+  };
 }
 
