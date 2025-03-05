81cdb6c6ca6935b555ed572c8388fa7ba4d22077
Ev357 <evest.dev@gmail.com>
2025-03-05 15:33:47 +0100

    refactor: removed arduino15

--- .config/home-manager/neovim/plugins/lsp.nix
+++ .config/home-manager/neovim/plugins/lsp.nix
@@ -11,14 +11,11 @@
       tailwindcss.enable = true;
       biome.enable = true;
       csharp_ls.enable = true;
-      # csharpier.enable = true;
       cssls.enable = true;
       emmet_language_server.enable = true;
       html.enable = true;
       lua_ls.enable = true;
-      # omnisharp.enable = true;
       pylsp.enable = true;
-      # stylua3p_ls.enable = true;
       svelte.enable = true;
       dockerls.enable = true;
       golangci_lint_ls.enable = true;
