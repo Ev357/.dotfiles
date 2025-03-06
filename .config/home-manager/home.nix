03bb31271a3bc0a69945844acbca45583c189f36
Ev357 <evest.dev@gmail.com>
2025-03-06 10:49:51 +0100

    refactor: mpv conf to hm

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -26,6 +26,7 @@
     ".config/electron-flags.conf".source = ./dotfiles/.config/electron-flags.conf;
     ".config/backgrounds/".source = ./dotfiles/.config/backgrounds;
     ".config/OpenTabletDriver/Presets/".source = ./dotfiles/.config/OpenTabletDriver/Presets;
+    ".config/mpv/mpv.conf".source = ./dotfiles/.config/mpv/mpv.conf;
     ".icons/hypr_Bibata-Modern-Classic/".source = builtins.fetchTarball {
       url = "https://github.com/LOSEARDES77/Bibata-Cursor-hyprcursor/releases/download/1.0/hypr_Bibata-Modern-Classic.tar.gz";
       sha256 = "08q5l2sywc0s70zdn7jvr0rbzz0w8j18wmlkf2x1l64y93lbvzsj";
@@ -41,6 +42,7 @@
     git = import ./programs/git;
     btop = import ./programs/btop;
     bat.enable = true;
+    mpv.enable = true;
     fzf = {
       enable = true;
       enableZshIntegration = true;
