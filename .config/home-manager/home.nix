f5175af2aa1351dfdad6ea8ccb2ef6dafd5d9898
Ev357 <evest.dev@gmail.com>
2025-03-08 17:11:36 +0100

    refactor: moved hyprpanel config to hm

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -28,6 +28,7 @@
     ".config/OpenTabletDriver/Presets/".source = ./dotfiles/.config/OpenTabletDriver/Presets;
     ".config/mpv/mpv.conf".source = ./dotfiles/.config/mpv/mpv.conf;
     ".config/zed/settings.json".source = ./dotfiles/.config/zed/settings.json;
+    ".config/hyprpanel/config.json".source = ./dotfiles/.config/hyprpanel/config.json;
     ".icons/hypr_Bibata-Modern-Classic/".source = builtins.fetchTarball {
       url = "https://github.com/LOSEARDES77/Bibata-Cursor-hyprcursor/releases/download/1.0/hypr_Bibata-Modern-Classic.tar.gz";
       sha256 = "08q5l2sywc0s70zdn7jvr0rbzz0w8j18wmlkf2x1l64y93lbvzsj";
