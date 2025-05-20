6705445e2e90de843eb6b334ab72bc687fca902e
Ev357 <evest.dev@gmail.com>
2025-05-20 16:57:23 +0200

    fix: allow unfree packages

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
   outputs = { nixpkgs, nixvim, home-manager, stylix, ... }@inputs:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
     in
     {
       homeConfigurations."evest" = home-manager.lib.homeManagerConfiguration {
