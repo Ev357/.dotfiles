8550c8da65ebaa1c6cbf377df694f979088d2f87
Ev357 <evest.dev@gmail.com>
2025-05-26 22:56:32 +0200

    refactor: telescope keymaps

--- modules/shell/nixvim/plugins/telescope.nix
+++ modules/shell/nixvim/plugins/telescope.nix
@@ -7,32 +7,6 @@
           sort_lastused = true;
         };
       };
-      keymaps = {
-        "<leader>sf" = {
-          action = "find_files";
-          options.desc = "[S]earch [F]iles";
-        };
-        "<leader>sg" = {
-          action = "live_grep";
-          options.desc = "[S]earch by [G]rep";
-        };
-        "<leader>sd" = {
-          action = "diagnostics";
-          options.desc = "[S]earch [D]iagnostics";
-        };
-        "<leader><leader>" = {
-          action = "buffers";
-          options.desc = "[ ] Find existing buffers";
-        };
-        "<leader>gb" = {
-          action = "git_branches";
-          options.desc = "[G]it [b]ranches";
-        };
-        "<leader>/" = {
-          action = "current_buffer_fuzzy_find";
-          options.desc = "[/] Fuzzily search in current buffer";
-        };
-      };
       extensions = {
         fzf-native.enable = true;
         ui-select.enable = true;
@@ -49,6 +23,54 @@
           end
         '';
         options.desc = "[S]earch [N]eovim files";
+      }
+      {
+        mode = "n";
+        key = "<leader>sf";
+        action.__raw = /*lua*/ ''
+          require('telescope.builtin').find_files
+        '';
+        options.desc = "[S]earch [F]iles";
+      }
+      {
+        mode = "n";
+        key = "<leader>sg";
+        action.__raw = /*lua*/ ''
+          require('telescope.builtin').live_grep
+        '';
+        options.desc = "[S]earch by [G]rep";
+      }
+      {
+        mode = "n";
+        key = "<leader>sd";
+        action.__raw = /*lua*/ ''
+          require('telescope.builtin').diagnostics
+        '';
+        options.desc = "[S]earch [D]iagnostics";
+      }
+      {
+        mode = "n";
+        key = "<leader><leader>";
+        action.__raw = /*lua*/ ''
+          require('telescope.builtin').buffers
+        '';
+        options.desc = "[ ] Find existing buffers";
+      }
+      {
+        mode = "n";
+        key = "<leader>gb";
+        action.__raw = /*lua*/ ''
+          require('telescope.builtin').git_branches
+        '';
+        options.desc = "[G]it [b]ranches";
+      }
+      {
+        mode = "n";
+        key = "<leader>/";
+        action.__raw = /*lua*/ ''
+          require('telescope.builtin').current_buffer_fuzzy_find
+        '';
+        options.desc = "[/] Fuzzily search in current buffer";
       }
     ];
   };
