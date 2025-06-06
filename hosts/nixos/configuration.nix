f61742e5b1f467f2ad0d18f40c811dbff2a01168
Ev357 <evest.dev@gmail.com>
2025-06-06 17:52:17 +0200

    feat: added other nix caches

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -60,8 +60,16 @@
   nix = {
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
-      extra-substituters = [ "https://hyprland.cachix.org" ];
-      extra-trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      extra-substituters = [
+        "https://ev357.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "ev357.cachix.org-1:bI65rULXWJ8IMM+tosc/Z+9W53nL6uj4+5FLXX6BN3Q="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
       auto-optimise-store = true;
     };
     gc = {
