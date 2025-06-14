da7db588bb270e83222e020d5172ab6a24adce97
Ev357 <evest.dev@gmail.com>
2025-06-14 16:18:15 +0200

    style: combined services

--- nixosModules/services/nextcloud/default.nix
+++ nixosModules/services/nextcloud/default.nix
@@ -6,29 +6,31 @@
   ];
 
   config = lib.mkIf config.services.nextcloud.enable {
-    services.nextcloud = {
-      https = true;
-      hostName = "nextcloud";
-      config = {
-        dbtype = "sqlite";
-        adminpassFile = "/etc/nextcloud-admin-pass";
-      };
-      settings = {
-        trusted_domains = [ "nextcloud.evest.dev" ];
-      };
-      extraApps = {
-        theming_customcss = pkgs.fetchNextcloudApp {
-          url = "https://github.com/nextcloud/theming_customcss/releases/download/v1.18.0/theming_customcss.tar.gz";
-          hash = "sha256-MsF+im9yCt7bRNIE8ait0wxcVzMXsHMNbp+IIzY/zJI=";
-          license = "agpl3Plus";
+    services = {
+      nextcloud = {
+        https = true;
+        hostName = "nextcloud";
+        config = {
+          dbtype = "sqlite";
+          adminpassFile = "/etc/nextcloud-admin-pass";
+        };
+        settings = {
+          trusted_domains = [ "nextcloud.evest.dev" ];
+        };
+        extraApps = {
+          theming_customcss = pkgs.fetchNextcloudApp {
+            url = "https://github.com/nextcloud/theming_customcss/releases/download/v1.18.0/theming_customcss.tar.gz";
+            hash = "sha256-MsF+im9yCt7bRNIE8ait0wxcVzMXsHMNbp+IIzY/zJI=";
+            license = "agpl3Plus";
+          };
         };
       };
-    };
 
-    services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
-      listen = [
-        { addr = "0.0.0.0"; port = 3080; }
-      ];
+      nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+        listen = [
+          { addr = "0.0.0.0"; port = 3080; }
+        ];
+      };
     };
 
     networking.firewall.allowedTCPPorts = [ 3080 ];
