812ae4638d90b1d1ddd7e515ffcc78f0b09b8681
Ev357 <evest.dev@gmail.com>
2025-06-19 15:58:21 +0200

    refactor: hided some zellij stuff

--- modules/shell/zellij/default.nix
+++ modules/shell/zellij/default.nix
@@ -1,5 +1,10 @@
 {
   programs.zellij.settings = {
     theme = "catppuccin-macchiato";
+    default_layout = "compact";
+    simplified_ui = true;
+    show_release_notes = false;
+    show_startup_tips = false;
+    pane_frames = false;
   };
 }
