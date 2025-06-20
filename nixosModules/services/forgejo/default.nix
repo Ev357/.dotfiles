fa378a884b61c2d0a7146ed2516670202c47dad0
Ev357 <evest.dev@gmail.com>
2025-06-20 11:12:08 +0200

    feat: forgejo catppuccin theme

--- nixosModules/services/forgejo/default.nix
+++ nixosModules/services/forgejo/default.nix
@@ -1,8 +1,14 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   cfg = config.services.forgejo;
   srv = cfg.settings.server;
+
+  theme = pkgs.fetchzip {
+    url = "https://github.com/catppuccin/gitea/releases/download/v1.0.2/catppuccin-gitea.tar.gz";
+    sha256 = "sha256-rZHLORwLUfIFcB6K9yhrzr+UwdPNQVSadsw6rg8Q7gs=";
+    stripRoot = false;
+  };
 in
 {
   config = lib.mkIf config.services.forgejo.enable {
@@ -20,8 +26,18 @@ in
           ENABLED = true;
           DEFAULT_ACTIONS_URL = "github";
         };
+        ui = {
+          THEMES = "catppuccin-macchiato-lavender";
+          DEFAULT_THEME = "catppuccin-macchiato-lavender";
+        };
       };
     };
+
+    systemd.services.forgejo.preStart = lib.mkAfter /* bash */ ''
+      rm -rf ${cfg.stateDir}/custom/public/assets
+      mkdir -p ${cfg.stateDir}/custom/public/assets
+      ln -sf ${theme} ${cfg.stateDir}/custom/public/assets/css
+    '';
 
     networking.firewall.allowedTCPPorts = [ srv.HTTP_PORT ];
   };
