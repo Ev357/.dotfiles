a94e42160adadbae7363541e08984cf335c5abe2
Ev357 <evest.dev@gmail.com>
2025-07-27 00:07:42 +0200

    perf: used fixed official cattpuccin-fcitx5

--- modules/fcitx5/default.nix
+++ modules/fcitx5/default.nix
@@ -1,44 +1,49 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 {
   imports = [
     ./patch
   ];
 
-  config = lib.mkIf config.i18n.inputMethod.enable
-    {
-      i18n.inputMethod = {
-        type = "fcitx5";
-        fcitx5 = {
-          addons = with pkgs; [
-            fcitx5-mozc
-            fcitx5-gtk
-          ];
-          waylandFrontend = true;
-          settings = import ./settings.nix;
-        };
-      };
-
-      home.file.".local/share/fcitx5/themes".source = pkgs.callPackage ./catppuccin-fcitx5 { };
-
-      wayland.windowManager.hyprland.environmentVariables = {
-        XMODIFIERS = "@im=fcitx";
-        QT_IM_MODULE = "fcitx";
-        GTK_IM_MODULE = "wayland";
-        QT_IM_MODULES = "wayland;fcitx;ibus";
-        SDL_IM_MODULE = "fcitx";
-      };
-
-      gtk = {
-        gtk2.extraConfig = /* ini */ ''
-          gtk-im-module="fcitx"
-        '';
-        gtk3.extraConfig = {
-          gtk-im-module = "fcitx";
-        };
-        gtk4.extraConfig = {
-          gtk-im-module = "fcitx";
-        };
+  config = lib.mkIf config.i18n.inputMethod.enable {
+    i18n.inputMethod = {
+      type = "fcitx5";
+      fcitx5 = {
+        addons = with pkgs; [
+          fcitx5-mozc
+          fcitx5-gtk
+        ];
+        waylandFrontend = true;
+        settings = import ./settings.nix;
       };
     };
+
+    home.file.".local/share/fcitx5/themes".source = "${pkgs.catppuccin-fcitx5}/share/fcitx5/themes";
+
+    wayland.windowManager.hyprland.environmentVariables = {
+      XMODIFIERS = "@im=fcitx";
+      QT_IM_MODULE = "fcitx";
+      GTK_IM_MODULE = "wayland";
+      QT_IM_MODULES = "wayland;fcitx;ibus";
+      SDL_IM_MODULE = "fcitx";
+    };
+
+    gtk = {
+      gtk2.extraConfig = # ini
+        ''
+          gtk-im-module="fcitx"
+        '';
+      gtk3.extraConfig = {
+        gtk-im-module = "fcitx";
+      };
+      gtk4.extraConfig = {
+        gtk-im-module = "fcitx";
+      };
+    };
+  };
 }
--- modules/fcitx5/patch/default.nix
+++ modules/fcitx5/patch/default.nix
@@ -1,4 +1,9 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 
 let
   cfg = config.i18n.inputMethod;
@@ -7,9 +12,25 @@ in
   config = lib.mkIf cfg.enable {
     nixpkgs.overlays = [
       (final: prev: {
-        libsForQt5 = prev.libsForQt5.overrideScope (nfinal: nprev: {
-          fcitx5-with-addons = pkgs.callPackage ./fcitx5-with-addons { };
-        });
+        libsForQt5 = prev.libsForQt5.overrideScope (
+          nfinal: nprev: {
+            fcitx5-with-addons = pkgs.callPackage ./fcitx5-with-addons { };
+          }
+        );
+
+        catppuccin-fcitx5 = prev.catppuccin-fcitx5.overrideAttrs {
+          installPhase = # bash
+            ''
+              runHook preInstall
+
+              find src -name theme.conf -exec sed -i 's/^# \(Image=\(panel\|highlight\)\.svg\)/\1/' {} +
+
+              mkdir -p $out/share/fcitx5
+              cp -r src $out/share/fcitx5/themes
+
+              runHook postInstall
+            '';
+        };
       })
     ];
   };
