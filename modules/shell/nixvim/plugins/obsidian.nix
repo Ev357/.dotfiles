268f9eba0243d0e066fef8681255e674987e97cf
Ev357 <evest.dev@gmail.com>
2025-08-10 17:03:54 +0200

    refactor: disabled obsidian nvim footer

--- modules/shell/nixvim/plugins/obsidian.nix
+++ modules/shell/nixvim/plugins/obsidian.nix
@@ -4,20 +4,22 @@
       workspaces = [
         {
           name = "no-vault";
-          path.__raw = /* lua */ ''
-            function()
-              return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
-            end
-          '';
-          overrides = {
-            new_notes_location = "current_dir";
-            disable_frontmatter.__raw = /* lua */ ''
+          path.__raw = # lua
+            ''
               function()
-                local buf_path = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
-                local obsidian_path = vim.fn.expand("~/Documents/obsidian")
-                return vim.fn.stridx(buf_path, obsidian_path) ~= 0
+                return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
               end
             '';
+          overrides = {
+            new_notes_location = "current_dir";
+            disable_frontmatter.__raw = # lua
+              ''
+                function()
+                  local buf_path = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+                  local obsidian_path = vim.fn.expand("~/Documents/obsidian")
+                  return vim.fn.stridx(buf_path, obsidian_path) ~= 0
+                end
+              '';
           };
         }
       ];
@@ -26,6 +28,7 @@
         min_chars = 2;
       };
       legacy_commands = false;
+      footer.enabled = false;
     };
   };
 }
