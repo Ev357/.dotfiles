ff305a6f7de3e061f7c2ee96e8979e0180226f4b
Ev357 <evest.dev@gmail.com>
2025-09-06 00:58:16 +0200

    refactor: used twilight version + configured zen for default stuff

--- modules/applications/zen-browser/default.nix
+++ modules/applications/zen-browser/default.nix
@@ -1,12 +1,48 @@
-{inputs, ...}: {
+{
+  lib,
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
   imports = [
-    inputs.zen-browser.homeModules.beta
+    inputs.zen-browser.homeModules.twilight
   ];
 
-  programs.zen-browser = {
-    policies = {
-      DisableAppUpdate = true;
-      DisableTelemetry = true;
+  config = lib.mkIf config.programs.zen-browser.enable {
+    programs.zen-browser = {
+      policies = {
+        DisableAppUpdate = true;
+        DisableTelemetry = true;
+      };
+    };
+
+    xdg.mimeApps = let
+      zen-browser = inputs.zen-browser.packages.${pkgs.system}.twilight;
+      value = zen-browser.meta.desktopFileName;
+
+      associations = builtins.listToAttrs (map (name: {
+          inherit name value;
+        }) [
+          "application/x-extension-shtml"
+          "application/x-extension-xhtml"
+          "application/x-extension-html"
+          "application/x-extension-xht"
+          "application/x-extension-htm"
+          "x-scheme-handler/unknown"
+          "x-scheme-handler/mailto"
+          "x-scheme-handler/chrome"
+          "x-scheme-handler/about"
+          "x-scheme-handler/https"
+          "x-scheme-handler/http"
+          "application/xhtml+xml"
+          "application/json"
+          "text/plain"
+          "text/html"
+        ]);
+    in {
+      associations.added = associations;
+      defaultApplications = associations;
     };
   };
 }
