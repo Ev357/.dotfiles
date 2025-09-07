57a7b37968745610a85bbf3d3dec6ca2a2d42d0e
Ev357 <evest.dev@gmail.com>
2025-09-07 21:42:38 +0200

    fix: hyprshot save directory

--- modules/hyprland/hyprshot/default.nix
+++ modules/hyprland/hyprshot/default.nix
@@ -1,6 +1,16 @@
-{pkgs, ...}: {
-  programs.hyprshot = {
-    package = pkgs.nur.repos.Ev357.hyprshot;
-    saveLocation = "$HOME/Pictures/screenshots";
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  config = lib.mkIf config.programs.hyprshot.enable {
+    programs.hyprshot = {
+      package = pkgs.nur.repos.Ev357.hyprshot;
+    };
+
+    modules.uwsm.environmentVariables = {
+      HYPRSHOT_DIR = "$HOME/Pictures/screenshots";
+    };
   };
 }
