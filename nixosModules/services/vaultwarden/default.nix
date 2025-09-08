bdd6bd1e849ea65f5234908628af65faa9532d3e
Ev357 <evest.dev@gmail.com>
2025-09-08 22:25:21 +0200

    feat: vaultwarden backups

--- nixosModules/services/vaultwarden/default.nix
+++ nixosModules/services/vaultwarden/default.nix
@@ -4,14 +4,22 @@
   ...
 }: {
   config = lib.mkIf config.services.vaultwarden.enable {
-    services.vaultwarden.config = {
-      ROCKET_ADDRESS = "::1";
-      ROCKET_PORT = 8222;
-      SIGNUPS_ALLOWED = false;
+    services.vaultwarden = {
+      config = {
+        ROCKET_ADDRESS = "::1";
+        ROCKET_PORT = 8222;
+        SIGNUPS_ALLOWED = false;
+      };
+
+      backupDir = "/data/backups/vaultwarden";
     };
 
     networking.firewall.allowedTCPPorts = [
       config.services.vaultwarden.config.ROCKET_PORT
     ];
+
+    users.users."vaultwarden" = {
+      extraGroups = ["media"];
+    };
   };
 }
