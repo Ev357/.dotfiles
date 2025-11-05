df6ab2d5adcd464f8d9c6ac86846682bbc2c3c71
Ev357 <evest.dev@gmail.com>
2025-11-05 18:46:45 +0100

    fix: GIMMIE MY SHIGGY BACK

--- modules/applications/vesktop/default.nix
+++ modules/applications/vesktop/default.nix
@@ -1,19 +1,27 @@
 {
-  programs.vesktop = {
-    settings = {
-      discordBranch = "canary";
-      minimizeToTray = false;
-      arRPC = true;
-      splashColor = "rgb(239, 239, 240)";
-      splashBackground = "rgb(32, 32, 36)";
-    };
-    vencord = {
-      themes = {
-        macchiato = ./macchiato.theme.css;
-      };
+  lib,
+  config,
+  ...
+}: {
+  config = lib.mkIf config.programs.vesktop.enable {
+    programs.vesktop = {
       settings = {
-        enabledThemes = ["macchiato.css"];
+        discordBranch = "canary";
+        minimizeToTray = false;
+        arRPC = true;
+        splashColor = "rgb(239, 239, 240)";
+        splashBackground = "rgb(32, 32, 36)";
+      };
+      vencord = {
+        themes = {
+          macchiato = ./macchiato.theme.css;
+        };
+        settings = {
+          enabledThemes = ["macchiato.css"];
+        };
       };
     };
+
+    xdg.configFile."vesktop/userAssets/splash".source = ./shiggy.gif;
   };
 }
