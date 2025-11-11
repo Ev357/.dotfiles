1d70f4d9e360a8f05db8cdf118a4a9d6db85c644
Ev357 <evest.dev@gmail.com>
2025-11-11 15:49:38 +0100

    refactor: customized gemini

--- modules/shell/gemini-cli/default.nix
+++ modules/shell/gemini-cli/default.nix
@@ -6,6 +6,12 @@
       disableAutoUpdate = true;
       disableUpdateNag = true;
     };
+    ui = {
+      hideBanner = true;
+      hideFooter = true;
+      hideTips = true;
+      useFullWidth = true;
+    };
     security = {
       auth = {
         selectedType = "oauth-personal";
