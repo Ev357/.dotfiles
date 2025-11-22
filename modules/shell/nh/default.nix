574da7475e157b8149660acbeeed2286341bfd0d
Ev357 <evest.dev@gmail.com>
2025-11-22 14:39:44 +0100

    feat: nh clean

--- modules/shell/nh/default.nix
+++ modules/shell/nh/default.nix
@@ -1,9 +1,18 @@
-{config, ...}: {
+{
+  lib,
+  config,
+  ...
+}: {
   imports = [
     ./patch
   ];
 
   programs.nh = {
     flake = "${config.home.homeDirectory}/.config/home-manager";
+
+    clean = {
+      enable = true;
+      extraArgs = "--keep 5 --keep-since 14d";
+    };
   };
 }
