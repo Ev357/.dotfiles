fa8309918e73b176cbb483c9d382051656a4fbf8
Ev357 <evest.dev@gmail.com>
2025-12-06 15:29:37 +0100

    feat: uwsm app prefix for launcher

--- modules/hyprland/hyprlauncher/default.nix
+++ modules/hyprland/hyprlauncher/default.nix
@@ -7,6 +7,7 @@
     package = inputs.hyprlauncher.packages.${pkgs.stdenv.hostPlatform.system}.default;
     settings = {
       ui.window_size = "600 360";
+      finders.desktop_launch_prefix = "uwsm app -- ";
     };
   };
 }
