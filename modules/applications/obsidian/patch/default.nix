838f0d417d49ecb7f2ffd6367549f90aca5b839b
Ev357 <evest.dev@gmail.com>
2026-01-31 21:20:01 +0100

    build: updated deps

--- modules/applications/obsidian/patch/default.nix
+++ modules/applications/obsidian/patch/default.nix
@@ -18,28 +18,36 @@ in {
     let
       vaults = builtins.filter (vault: vault.enable == true) (builtins.attrValues cfg.vaults);
     in {
-      xdg.configFile."obsidian/obsidian.json".source =
-        lib.mkForce
-        (
-          (pkgs.formats.json {}).generate "obsidian.json" ({
-              vaults = builtins.listToAttrs (
-                builtins.map
-                (vault: {
-                  name = builtins.hashString "md5" vault.target;
-                  value =
-                    {
-                      path = "${config.home.homeDirectory}/${vault.target}";
-                    }
-                    // (lib.attrsets.optionalAttrs ((builtins.length vaults) == 1) {
-                      open = true;
-                    });
-                })
-                vaults
-              );
-              updateDisabled = true;
-            }
-            // cfg.extraSettings)
-        );
+      home.activation.obsidian = let
+        template = (pkgs.formats.json {}).generate "obsidian.json" ({
+            vaults = builtins.listToAttrs (
+              map (vault: {
+                name = builtins.substring 0 16 (builtins.hashString "md5" vault.target);
+                value = {
+                  path = "${config.home.homeDirectory}/${vault.target}";
+                };
+              })
+              vaults
+            );
+            updateDisabled = true;
+          }
+          // cfg.extraSettings);
+      in
+        lib.mkForce (lib.hm.dag.entryAfter ["writeBoundary"]
+          # bash
+          ''
+            OBSIDIAN_CONFIG="$HOME/.config/obsidian/obsidian.json"
+            if [ -f "$OBSIDIAN_CONFIG" ]; then
+              verboseEcho "Merging existing Obsidian config with generated template"
+              tmp="$(mktemp)"
+              run ${lib.getExe pkgs.jq} -s '(.[0] // {}) * (.[1] // {})' "$OBSIDIAN_CONFIG" "${template}" > "$tmp"
+              run install -m644 "$tmp" "$OBSIDIAN_CONFIG"
+              rm -f "$tmp"
+            else
+              verboseEcho "Installing fresh Obsidian config"
+              run install -D -m644 "${template}" "$OBSIDIAN_CONFIG"
+            fi
+          '');
     }
   );
 }
