7a6eac3e30e923d369f18f9188ba259e79416b51
Ev357 <evest.dev@gmail.com>
2026-02-12 00:52:05 +0100

    refactor: configured qbittorrent

--- nixosModules/services/qbittorrent/default.nix
+++ nixosModules/services/qbittorrent/default.nix
@@ -6,7 +6,9 @@
   services.qbittorrent = {
     package = inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.qbittorrent-nox;
     webuiPort = 8087;
+    torrentingPort = 51820;
     openFirewall = true;
     group = "media";
+    profileDir = "/data/services/qbittorrent";
   };
 }
