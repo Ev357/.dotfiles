6377653a1bf07eac786e5fdd332627879ac508c3
Ev357 <evest.dev@gmail.com>
2026-03-01 02:13:37 +0100

    feat: a working vicinae bitwarden extension

--- modules/widgets/vicinae/default.nix
+++ modules/widgets/vicinae/default.nix
@@ -1,14 +1,22 @@
-{
+{pkgs, ...}: {
   programs.vicinae = {
     settings = {
       theme = {
         dark.name = "catppuccin-macchiato";
         light.name = "catppuccin-macchiato";
       };
+      providers = {
+        "@jomifepe/${pkgs.nur.repos.Ev357.raycast-bitwarden.name}" = {
+          preferences = {
+            cliPath = "${pkgs.bitwarden-cli}/bin/bw";
+            serverUrl = "https://vaultwarden.ts.evest.dev";
+          };
+        };
+      };
     };
     systemd.enable = true;
-    # extensions = with pkgs; [
-    #   nur.repos.Ev357.bitwarden
-    # ];
+    extensions = with pkgs; [
+      nur.repos.Ev357.raycast-bitwarden
+    ];
   };
 }
