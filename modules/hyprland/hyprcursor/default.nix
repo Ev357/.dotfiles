d25c0777832da817faf217bfb45075fe2272d68b
Ev357 <evest.dev@gmail.com>
2026-05-11 14:54:36 +0200

    style: removed another useless builtin

--- modules/hyprland/hyprcursor/default.nix
+++ modules/hyprland/hyprcursor/default.nix
@@ -8,7 +8,7 @@
   };
 
   config = lib.mkIf config.modules.hyprland.hyprcursor.enable {
-    home.file.".icons/hypr_Bibata-Modern-Classic/".source = builtins.fetchTarball {
+    home.file.".icons/hypr_Bibata-Modern-Classic/".source = fetchTarball {
       url = "https://github.com/LOSEARDES77/Bibata-Cursor-hyprcursor/releases/download/1.0/hypr_Bibata-Modern-Classic.tar.gz";
       sha256 = "08q5l2sywc0s70zdn7jvr0rbzz0w8j18wmlkf2x1l64y93lbvzsj";
     };
