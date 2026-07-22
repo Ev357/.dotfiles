1ac267bb68b67adf0f009d7d4f23769a245a1232
Ev357 <evest.dev@gmail.com>
2026-07-23 01:42:16 +0200

    refactor: removed DLNA from raspberrypi

--- hosts/raspberrypi/nixosModules/services/jellyfin/default.nix
+++ hosts/raspberrypi/nixosModules/services/jellyfin/default.nix
@@ -1,19 +1,10 @@
 {
-  lib,
-  config,
   pkgs,
   inputs,
   ...
 }: {
-  config = lib.mkIf config.services.jellyfin.enable {
-    services.jellyfin = {
-      package = inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.jellyfin;
-      group = "media";
-    };
-
-    # DLNA
-    networking.firewall.allowedUDPPorts = [
-      1900
-    ];
+  services.jellyfin = {
+    package = inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.jellyfin;
+    group = "media";
   };
 }
