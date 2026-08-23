3240eb4d9333d24e89523b1a0c1b92bffedcc375
Ev357 <evest.dev@gmail.com>
2026-08-23 18:00:33 +0200

    feat: web access and ponytail

--- modules/shell/pi-coding-agent/default.nix
+++ modules/shell/pi-coding-agent/default.nix
@@ -20,6 +20,8 @@ in {
 
       settings = {
         npmCommand = ["bun"];
+        defaultProvider = "antigravity";
+        defaultModel = "claude-opus-4-6";
         theme = "catppuccin-tui-macchiato";
         quietStartup = true;
         enableInstallTelemetry = false;
@@ -28,8 +30,10 @@ in {
         packages = [
           "npm:pi-antigravity"
           "npm:pi-image-tools"
+          "npm:pi-web-access"
           "git:github.com/Ev357/pi-vimmode@fix-agent-directory-config"
           "npm:pi-catppuccin-tui"
+          "git:github.com/DietrichGebert/ponytail"
         ];
 
         piVimMode = {
@@ -46,13 +50,24 @@ in {
       };
     };
 
-    home.file."${cfg.configDir}/npm/node_modules/pi-image-tools/config/config.json" = {
-      text = builtins.toJSON {
-        debug = false;
-        shortcuts = {
-          pasteImage = ["ctrl+v"];
-          avoidBuiltinConflicts = true;
-          suppressBuiltinConflictWarnings = true;
+    home.file = {
+      "${cfg.configDir}/npm/node_modules/pi-image-tools/config/config.json" = {
+        text = builtins.toJSON {
+          debug = false;
+          shortcuts = {
+            pasteImage = ["ctrl+v"];
+            avoidBuiltinConflicts = true;
+            suppressBuiltinConflictWarnings = true;
+          };
+        };
+      };
+      "${config.xdg.configHome}/ponytail/config.json".text = builtins.toJSON {
+        quietStartup = true;
+      };
+      "${cfg.configDir}/web-search.json" = {
+        text = builtins.toJSON {
+          autoOpenBrowser = false;
+          workflow = "auto-summary";
         };
       };
     };
