37e8aa90d4053cf370d7ffd1a6f786532d2f9a8d
Ev357 <evest.dev@gmail.com>
2026-09-03 19:42:38 +0200

    refactor: changed to openflowlm

--- nixosModules/hardware/amd-npu/default.nix
+++ nixosModules/hardware/amd-npu/default.nix
@@ -4,14 +4,6 @@
     inputs.nix-amd-ai.nixosModules.default
   ];
 
-  nixpkgs.overlays = [
-    (_: prev: {
-      fastflowlm = prev.callPackage ./fastflowlm.nix {
-        fastflowlm = prev.fastflowlm;
-      };
-    })
-  ];
-
   hardware.amd-npu = {
     enableVulkan = true;
     enableLemonade = false;
--- nixosModules/hardware/amd-npu/patch/default.nix
+++ nixosModules/hardware/amd-npu/patch/default.nix
@@ -1,11 +1,18 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   cfg = config.hardware.amd-npu;
 in {
   config = lib.mkIf cfg.enable {
     environment.pathsToLink = lib.optionals cfg.enableFastFlowLM ["/share/flm"];
+
+    nixpkgs.overlays = lib.mkIf cfg.enableFastFlowLM [
+      (_: _: {
+        fastflowlm = pkgs.nur.repos.Ev357.openflowlm;
+      })
+    ];
   };
 }
